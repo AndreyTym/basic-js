4b516fef8c06d3158c36984c856f3f7f89851133
Andrey <tymoshenko.andrey@gmail.com>
2020-09-15 23:31:12 +0300

    fix: count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,8 +1,10 @@
 module.exports = function countCats(backyard) {
   let number = 0;
   for (let i = 0; i < backyard.length; i++) {
-    for (let j = 0; j < backyard.length; j++) {
-      if (backyard[i][j] == "^^") number++;
+    for (let j = 0; j < backyard[i].length; j++) {
+      if (backyard[i][j] === "^^") {
+        number++;
+      }
     }
   }
   return number;
